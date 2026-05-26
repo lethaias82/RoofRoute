c1abb17b98dec362e0ff2cd994dd034aa2dbcd72
lethaias82 <lethaias82@gmail.com>
2026-05-26 12:46:32 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Let the browser handle all requests normally
+  // This service worker just allows the app to work offline
+});
